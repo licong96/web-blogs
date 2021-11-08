091518e68a6937abaabfd1e3709e1fe619e90a88
黎聪 <licong23@xdf.cn>
2021-11-08 16:23:21 +0800

    添加Stream拷贝文件

--- server/test.js
+++ server/test.js
@@ -1,24 +1,18 @@
 const fs = require('fs')
 const path = require('path')
-const fileName = path.resolve(__dirname, 'test.txt')
 
-// 读取文件内容
-fs.readFile(fileName, (err, data) => {
-	if (err) {
-		console.log(err)
-		return
-	}
-	// data 是二进制类型，需要转为字符串
-	console.log(data.toString())
+const fileName1 = path.resolve(__dirname, 'test1.txt')
+const fileName2 = path.resolve(__dirname, 'test2.txt')
+
+const readStream = fs.createReadStream(fileName1)
+const writeStream = fs.createWriteStream(fileName2)
+
+readStream.pipe(writeStream)
+
+readStream.on('data', chunk => {
+	console.log('pipe')
 })
 
-// 写入文件
-const content = '\n这是新写入的内容'
-const opt = {
-	flag: 'a'	// a: 追加写入，w: 覆盖写入
-}
-fs.writeFile(fileName, content, opt, (err) => {
-	if (err) {
-		console.log(err)
-	}
+readStream.on('end', () => {
+	console.log('copy done')
 })
